26ec950f4e9ad6fd41b5c7cb7962753fb27a6606
Jonathan Abreu <jonathan.pereira@prill.com.br>
2022-10-11 08:51:34 -0300

    Base do projeto

---
+++ src/main/java/br/com/jonathan/apiresfulpokemon/ApiResfulPokemonApplication.java
@@ -0,0 +1,13 @@
+package br.com.jonathan.apiresfulpokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResfulPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResfulPokemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jonathan/apiresfulpokemon/ApiResfulPokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jonathan.apiresfulpokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResfulPokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
